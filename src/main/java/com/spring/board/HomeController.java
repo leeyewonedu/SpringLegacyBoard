2bc175586b940672f355dc28d6d7b78e9f908e43
leeyewon <leeyewonedu@gmail.com>
2023-08-05 22:44:50 +0900

    init project

---
+++ src/main/java/com/spring/board/HomeController.java
@@ -0,0 +1,14 @@
+package com.spring.board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        //view의 이름
+        return "index";
+    }
+            
+}
